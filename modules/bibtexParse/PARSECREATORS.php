backdrop-contrib/biblio

commit 4e6fb96d05389151b0df935bf4aae458bb086601

Issue #2077127 by Liam Morland: Refactor query parameters in getCreatorByName()

modules/bibtexParse/PARSECREATORS.php
@@ -32,7 +32,7 @@ class Creators extends PARSECREATORS
     // [[:<:]], [[:>:]] These stand for word boundaries; see biblio.pages.inc.
     $result = db_query('SELECT *
                     FROM {biblio_contributor_data}
-                    WHERE lastname RLIKE "[[:<:]]%s[[:>:]]" ', $name);
+                    WHERE lastname RLIKE :lastname', array('lastname' => '[[:<:]]' . $value . '[[:>:]]'));
   }
 
   function getCreatorCount()

commit 5e3dac358cc383d071b24c4788bbda27d298235d

Issue #2077127 by Liam Morland: Correct variable name in getCreatorByName()
Fixes problem introduced in 4e6fb96.

modules/bibtexParse/PARSECREATORS.php
@@ -45,7 +45,7 @@ class Creators extends PARSECREATORS {
     // [[:<:]], [[:>:]] These stand for word boundaries; see biblio.pages.inc.
     $result = db_query('SELECT *
                     FROM {biblio_contributor_data}
-                    WHERE lastname RLIKE :lastname', array('lastname' => '[[:<:]]' . preg_quote($value) . '[[:>:]]'));
+                    WHERE lastname RLIKE :lastname', array('lastname' => '[[:<:]]' . preg_quote($name) . '[[:>:]]'));
   }
 
   /**